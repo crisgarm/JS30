6a1ff1aab9711dd9916b1b95255ee06d108f5cd3
crisgarm <crisgarm9@gmail.com>
2020-12-17 18:15:24 +0100

    Add exercise 05

--- 05. Flex Panels Image Gallery/main.js
+++ 05. Flex Panels Image Gallery/main.js
@@ -1,29 +1,19 @@
 "use strict";
 
-const checkboxes = document.querySelectorAll(".input__checkbox");
+const panels = document.querySelectorAll(".panel");
 
-//we create a variable that stores which element was checked (this)
-let lastChecked;
-
-function handleCheck(e) {
-  //we declare a flag variable and check if shift key was pressed down and checkbox has been checked
-  let inBetween = false;
-  if (e.shiftKey && this.checked) {
-    //loop over every single checkbox to know if the checkbox element is equal to lastChecked value or the element that was shift clicked, the flag variable is set to the opposite value
-    checkboxes.forEach((checkbox) => {
-      if (checkbox === this || checkbox === lastChecked) {
-        inBetween = !inBetween;
-      }
-      //If the flag variable is true, the checkbox is checked
-      if (inBetween) {
-        checkbox.checked = true;
-      }
-    });
-  }
-  lastChecked = this;
+function toggleOpen() {
+  this.classList.toggle("open");
+  console.log(this);
 }
 
-// we add an event listener for each element by looping through them
-checkboxes.forEach((checkbox) =>
-  checkbox.addEventListener("click", handleCheck)
+function toggleActive(ev) {
+  if (ev.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
 );
